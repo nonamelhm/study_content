21698d6f9c9ed899ffae72eb289469105982035d
nonamelhm <2628764605@qq.com>
2022-07-07 16:52:56 +0800

    flv

---
+++ src/components/Flv.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <video id="videoElement" controls autoplay></video>
+   <el-button type="primary" @click="getVideo()">开启直播</el-button>
+  </div>
+</template>
+<script>
+import flvjs from "flv.js"
+export default{
+  name:'Flv',
+  methods:{
+     getVideo(){
+	     let videoElement = document.getElementById('videoElement');
+       let flvPlayer = flvjs.createPlayer({
+            type: 'flv',
+            isLive: true,
+            url: 'http://video.flv'
+        });
+        flvPlayer.attachMediaElement(videoElement);
+        flvPlayer.load();
+        flvPlayer.play();
+    },
+  }
+}
+</script>
+<style scoped>
+#videoElement{
+  width:600px;
+  height:450px;
+}
+</style>
